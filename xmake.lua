ba8b75a6c45a0a20d235ed45d414d48613b51a14
Rikki <rikki@member.fsf.org>
2024-12-21 08:52:00 +0800

    update: add sqlite3 library

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,18 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 
-add_includedirs("src")
+add_includedirs("src","sqlite3")
+
+target("sqlite3")
+    set_kind("shared")
+    set_languages("c11")
+    set_optimize("fastest")
+    set_warnings("none")
+    add_files("sqlite3/**.c")
 
 target("media-ms")
     set_kind("binary")
     set_languages("c11")
     set_optimize("fastest")
+    add_deps("sqlite3")
     add_files("src/**.cpp")
